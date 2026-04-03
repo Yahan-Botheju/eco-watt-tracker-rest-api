95a37f173d9ca80fa3417e69833c621ebb4f12fe
Yahan Botheju <yahan.ravinga.git@gmail.com>
2026-04-03 12:19:13 +0530

    initial commit of java-spring-clean-architecture_electricity-consumption-system

---
+++ src/main/java/lk/simple/electricity_consumption_system/ElectricityConsumptionApplication.java
@@ -0,0 +1,13 @@
+package lk.simple.electricity_consumption_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectricityConsumptionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElectricityConsumptionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lk/simple/electricity_consumption_system/ElectricityConsumptionApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.simple.electricity_consumption_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectricityConsumptionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
