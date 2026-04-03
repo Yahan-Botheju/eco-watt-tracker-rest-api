b1f8fff9f4d4f03e26f933f27d9967db25c686af
Yahan Botheju <yahan.ravinga.git@gmail.com>
2026-04-03 13:43:55 +0530

    feat: create domain repository get all and save methods

--- src/main/java/lk/simple/electricity_consumption_system/domain/repository/ElectricityUsageRepository.java
+++ src/main/java/lk/simple/electricity_consumption_system/domain/repository/ElectricityUsageRepository.java
@@ -1,4 +1,14 @@
 package lk.simple.electricity_consumption_system.domain.repository;
 
+import lk.simple.electricity_consumption_system.domain.model.ElectricityUsage;
+
+import java.util.List;
+
 public interface ElectricityUsageRepository {
+
+    //get all usage
+    List<ElectricityUsage> getAllUsage();
+
+    //save usage
+    void saveUsage(ElectricityUsage electricityUsage);
 }
