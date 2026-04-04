9fee2fc2904576ce4894dfa6f6a004a8828f1bbf
Yahan Botheju <yahan.ravinga.git@gmail.com>
2026-04-04 14:08:37 +0530

    feat: inject model repo as constructor injection

--- src/main/java/lk/simple/electricity_consumption_system/usecase/ElectricityUsageUseCaseImpl.java
+++ src/main/java/lk/simple/electricity_consumption_system/usecase/ElectricityUsageUseCaseImpl.java
@@ -1,4 +1,14 @@
 package lk.simple.electricity_consumption_system.usecase;
 
-public class ElectricityUsageUseCaseImpl {
+import lk.simple.electricity_consumption_system.domain.repository.ElectricityUsageRepository;
+import lk.simple.electricity_consumption_system.infrastructure.persistence.ElectricityUsageImpl;
+
+public class ElectricityUsageUseCaseImpl implements ElectricityUsageUseCase{
+
+    //inject model repo as constructor
+    private final ElectricityUsageRepository electricityUsageRepository;
+
+    public ElectricityUsageUseCaseImpl(ElectricityUsageRepository electricityUsageRepository){
+        this.electricityUsageRepository = electricityUsageRepository;
+    }
 }
