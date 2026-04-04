9e77d7083a3779c4aaf228736176c7c7d50499d8
Yahan Botheju <yahan.ravinga.git@gmail.com>
2026-04-04 14:26:07 +0530

    feat: implement get all data method functionality

--- src/main/java/lk/simple/electricity_consumption_system/usecase/ElectricityUsageUseCaseImpl.java
+++ src/main/java/lk/simple/electricity_consumption_system/usecase/ElectricityUsageUseCaseImpl.java
@@ -1,7 +1,10 @@
 package lk.simple.electricity_consumption_system.usecase;
 
+import lk.simple.electricity_consumption_system.domain.model.ElectricityUsage;
 import lk.simple.electricity_consumption_system.domain.repository.ElectricityUsageRepository;
 import lk.simple.electricity_consumption_system.infrastructure.persistence.ElectricityUsageImpl;
+
+import java.util.List;
 
 public class ElectricityUsageUseCaseImpl implements ElectricityUsageUseCase{
 
@@ -10,5 +13,16 @@ public class ElectricityUsageUseCaseImpl implements ElectricityUsageUseCase{
 
     public ElectricityUsageUseCaseImpl(ElectricityUsageRepository electricityUsageRepository){
         this.electricityUsageRepository = electricityUsageRepository;
+    }
+
+    //get all data and return to domain repo
+    @Override
+    public List<ElectricityUsage> getAllUsage(){
+        return electricityUsageRepository.getAllUsage();
+    }
+
+    @Override
+    public void saveUsage(ElectricityUsage electricityUsage){
+        //logic not implemented yet
     }
 }
