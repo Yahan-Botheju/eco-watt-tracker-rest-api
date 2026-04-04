a65ad9abd6c9e4535b7c8fd0377cad9b0deff4a2
Yahan Botheju <yahan.ravinga.git@gmail.com>
2026-04-04 12:41:25 +0530

    feat: implement get all data mothod

--- src/main/java/lk/simple/electricity_consumption_system/infrastructure/persistence/ElectricityUsageImpl.java
+++ src/main/java/lk/simple/electricity_consumption_system/infrastructure/persistence/ElectricityUsageImpl.java
@@ -2,9 +2,11 @@ package lk.simple.electricity_consumption_system.infrastructure.persistence;
 
 import lk.simple.electricity_consumption_system.domain.model.ElectricityUsage;
 import lk.simple.electricity_consumption_system.domain.repository.ElectricityUsageRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public class ElectricityUsageImpl implements ElectricityUsageRepository {
 
     //inject jpa repository as constructor
@@ -14,5 +16,23 @@ public class ElectricityUsageImpl implements ElectricityUsageRepository {
         this.jpaElectricityUsageRepository = jpaElectricityUsageRepository;
     }
 
+    //get all data from db, show to user
+    @Override
+    public List<ElectricityUsage> getAllUsage(){
+
+        List<ElectricityUsageEntity> electricityUsage = jpaElectricityUsageRepository.findAll();
+
+       return electricityUsage.stream()
+               .map(entity -> new ElectricityUsage(
+                       entity.getId(),
+                       entity.getDate(),
+                       entity.getUnitConsumed(),
+                       entity.getCategory())).toList();
+    }
+
+    @Override
+    public void saveUsage(ElectricityUsage electricityUsage){
+        //not implement yet
+    }
 
 }
