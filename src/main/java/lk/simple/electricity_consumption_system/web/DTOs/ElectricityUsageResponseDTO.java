97c2954739a7a34c2eae458ea82e5b7042f1ef5b
Yahan Botheju <yahan.ravinga.git@gmail.com>
2026-04-04 16:55:38 +0530

    feat: create response dto template

--- src/main/java/lk/simple/electricity_consumption_system/web/DTOs/ElectricityUsageResponseDTO.java
+++ src/main/java/lk/simple/electricity_consumption_system/web/DTOs/ElectricityUsageResponseDTO.java
@@ -1,4 +1,17 @@
 package lk.simple.electricity_consumption_system.web.DTOs;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ElectricityUsageResponseDTO {
+    private Long id;
+    private LocalDate date;
+    private int unitConsumed;
+    private String category;
 }
