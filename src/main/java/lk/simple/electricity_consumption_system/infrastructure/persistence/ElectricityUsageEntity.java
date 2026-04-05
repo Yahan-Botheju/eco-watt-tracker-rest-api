082077c503000a53c01c1e857ee5b0e317d59bad
Yahan Botheju <yahan.ravinga.git@gmail.com>
2026-04-05 12:35:10 +0530

    feat: add soft delete for is deleted

--- src/main/java/lk/simple/electricity_consumption_system/infrastructure/persistence/ElectricityUsageEntity.java
+++ src/main/java/lk/simple/electricity_consumption_system/infrastructure/persistence/ElectricityUsageEntity.java
@@ -4,12 +4,16 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SoftDelete;
+import org.hibernate.annotations.Where;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "electricity_usage")
+@SoftDelete(columnName = "isDeleted")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
