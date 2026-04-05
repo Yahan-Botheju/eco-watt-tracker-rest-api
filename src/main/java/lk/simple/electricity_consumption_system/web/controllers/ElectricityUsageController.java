c6bc68473655db9b5f8ca8846b29ba219354de27
Yahan Botheju <yahan.ravinga.git@gmail.com>
2026-04-05 10:35:03 +0530

    feat: create get request

--- src/main/java/lk/simple/electricity_consumption_system/web/controllers/ElectricityUsageController.java
+++ src/main/java/lk/simple/electricity_consumption_system/web/controllers/ElectricityUsageController.java
@@ -1,12 +1,19 @@
 package lk.simple.electricity_consumption_system.web.controllers;
 
+import lk.simple.electricity_consumption_system.domain.model.ElectricityUsage;
 import lk.simple.electricity_consumption_system.usecase.ElectricityUsageUseCase;
+import lk.simple.electricity_consumption_system.web.DTOs.ElectricityUsageResponseDTO;
 import lk.simple.electricity_consumption_system.web.mappers.ElectricityUsageMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+@RestController
 @RequestMapping("/api/v1/usage")
 @RequiredArgsConstructor
 public class ElectricityUsageController {
@@ -16,4 +23,10 @@ public class ElectricityUsageController {
 
     //inject mapper
     private final ElectricityUsageMapper electricityUsageMapper;
+
+    //get all usages
+    @GetMapping
+    public List<ElectricityUsageResponseDTO> getAllUsage(){
+         return electricityUsageUseCase.getAllUsage().stream().map(electricityUsageMapper::toResponseDTO).toList();
+    }
 }
